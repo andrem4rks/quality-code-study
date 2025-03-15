1d5b3ebcc6a3f84717cac0ab853485fb848f7e5b
Andre Marks <andrem4rks@gmail.com>
2025-03-15 12:35:28 -0500

    add/singleton with enum pattern

---
+++ designpatterns/singleton/singletoneenum/test/SingletonLazyTest.java
@@ -0,0 +1,18 @@
+package designpatterns.singleton.singletoneenum.test;
+
+import designpatterns.singleton.singletoneenum.domain.AircraftSingletonEnum;
+
+public class SingletonLazyTest {
+    public static void main(String[] args) {
+
+        bookSeat("A1");
+        bookSeat("A1");
+
+    }
+
+    private static void bookSeat(String seat) {
+        AircraftSingletonEnum aircraft = AircraftSingletonEnum.INSTANCE;
+        System.out.println(aircraft.bookSeat(seat));
+        System.out.println(aircraft.hashCode());
+    }
+}
